771f59baf5bcfc57d28d5f605a87cf60fc8279fa
Kirill <kiril2306@gmail.com>
2023-04-16 22:58:36 +0300

    eh...already forgot what i've done, it works tho

--- matrix.cpp
+++ matrix.cpp
@@ -19,72 +19,8 @@ baseMatrix::baseMatrix(int rows_, int cols_, std::vector<number> input) {
     isUsed = true;
 }
 
-//void baseMatrix::make(std::vector<number> s,int isize) {
-//    size = isize;
-//    m.clear();
-//    int c = 0;
-//    for (size_t i = 0; i < size; ++i) {
-//        Row row(size);
-//        for (size_t j = 0; j < size; ++j) {
-//            row[j] = s[c];
-//            c++;
-//        }
-//        m.push_back(row);
-//    }
-//    isUsed = true;
-//}
-
-void baseMatrix::show() {
-    check();
-    for (size_t i = 0; i < rows; ++i)
-    {
-        for (size_t j = 0; j < cols; ++j) std::cout << m[i][j] << " ";
-        std::cout << std::endl;
-    }
-    return;
-}
-
-//number baseMatrix::findDet() {
-//    //check();
-//    det = 1;
-//    cc = m;//на всякий случай, работаем с копией матрицы
-//    for (int i = 0; i < size; i++) {
-//        int k = i;
-//        for (int j = i + 1; j < size; j++) {
-//            number a = cc[j][i], b = cc[k][i];
-//            if (abs(a) > abs(b))
-//                k = j;
-//        }
-//        cc[i].swap(cc[k]);//тут должны меняться строки матрицы
-//        if (i != k) det *= -1;
-//        det *= cc[i][i];
-//        for (int j = i + 1; j < size; j++) 
-//            cc[i][j] /= cc[i][i];
-//        for (int j = 0; j < size; j++)
-//            if ((j != i))
-//                for (k = i + 1; k < size; k++)
-//                    cc[j][k] -= cc[i][k] * cc[j][i];
-//    }
-//    //std::cout << det << std::endl;
-//    return det;
-//};
-
-void baseMatrix::transpon() {
-    check();
-    cc = m;
-    number cache;
-    for (int i = 0; i < rows; i++)
-        for (int j = i; j < cols; j++) {
-            cache = m[i][j];
-            m[i][j] = m[j][i];
-            m[j][i] = cache;
-        }
-    show();
-    m = cc;
-}
-
 QString baseMatrix::transpose() {
-    cc = m;
+    Matrix cc = m;
     number cache;
     for (int i = 0; i < rows; i++)
         for (int j = i; j < cols; j++) {
@@ -106,8 +42,8 @@ QString baseMatrix::transpose() {
 }
 
 int baseMatrix::ranking() {
-    check();
-    cc = m;
+    //check();
+    Matrix cc = m;
     int rank = min(rows, cols);
     std::vector<bool> line_used(rows);
     for (int i = 0; i < rows; i++) {
@@ -128,20 +64,3 @@ int baseMatrix::ranking() {
     return rank;
 }
 
-void baseMatrix::check() {
-    if (!isUsed) {
-        //make(3, true);
-        std::cout << "Не введена матрица, используется матрица по умолчанию\n";
-    }
-};
-
-//void baseMatrix::make(int s, bool based) {//метод когда у нас уже есть значения, should start with make(3,true)
-//    size_t x = 0; size = s;
-//    for (size_t i = 0; i < s; ++i)
-//    {
-//        Row row(s);
-//        for (size_t j = 0; j < s; ++j, ++x) row[j] = def[x];
-//        m.push_back(row);
-//    }
-//    isUsed = true;
-//};
--- matrix.h
+++ matrix.h
@@ -6,24 +6,14 @@
 class baseMatrix {
 public:
     baseMatrix(int rows_, int cols_, std::vector<number> input);
-    //void make(int s);
-    void make(std::vector<number>,int);
-    void show();
-    //number findDet(); going to descendant
-    void transpon();
     QString transpose();
     int ranking();//need to get initial rank from rows or cols
 protected:
     typedef std::vector<std::vector<number>> Matrix;
     typedef std::vector<number> Row; //can't put typedef to private while keeping Matrix in protected
-    Matrix m, cc;
+    Matrix m;
     int cols = 3;//for square matrix we need only one dimension to be visible
 private:
-    //number det = 1; going to descendant
-    //int size = 3, rank = size;
     int rows = 3;
     bool isUsed = false;//показывает иницилизирована ли матрица, если нет возьмем дефолтную
-    void check();
-    //number def[9] = { 1,2,3,4,1,6,7,8,1 };
-    //void make(int s, bool based);
 };
