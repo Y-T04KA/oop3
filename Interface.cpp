7cb4393da13395e76afed65f1cbe5b25d751cc06
Kirill <kiril2306@gmail.com>
2023-04-30 17:12:57 +0300

    made it slightly less cringey

--- Interface.cpp
+++ Interface.cpp
@@ -17,61 +17,8 @@ void TServerInterface::receive(QByteArray msg) {//msg should be like [size,mode,
 	batch_size = splitted[0].toInt();
 	mode = splitted[1].toInt();
 	std::cout << "batch_size - " << batch_size << "\nmode - " << mode << "\nsplitted size - " << splitted.size() << endl;
-	switch (mode)
-	{
-	case 1: {//deter for double
-		std::vector<myDouble> data;
-		for (int i = 2; i < 2 + batch_size*batch_size; i++) {
-			myDouble d = splitted.at(i).toDouble();
-			std::cout << d.get() << endl;
-			data.push_back(d);
-		}
-		SqMatrix<myDouble> sq(batch_size, data);
-		std::cout << "processed data from request\n" << data.size() << endl;
-		answer += "определитель вещественной матрицы - ";
-		s << sq.findDet();
-		answer += s;
-		std::cout << "Found determ - " << s.toStdString() << endl;
-		break;
-	}
-	case 2: {//deter for complex
-		std::vector<TComplex> data;
-		for (int i = 2; i < 2 + batch_size * batch_size * 2; i++) {
-			if (i % 2 == 0) {
-				re = splitted.at(i).toInt();
-			}
-			else {
-				im = splitted.at(i).toInt();
-				TComplex die(re, im);
-				data.push_back(die);
-			}
-		}
-		SqMatrix<TComplex> sq(batch_size, data);
-		answer += "определитель комплексной матрицы - ";
-		s << sq.findDet();
-		answer += s;
-	}
-		break;
-	
-	case 3: {//deter for rational
-		std::vector<TRational> data;
-		for (int i = 2; i < 2 + batch_size * batch_size * 2; i++) {
-			if (i % 2 == 0) {
-				re = splitted.at(i).toInt();
-			}
-			else {
-				im = splitted.at(i).toInt();
-				TRational die(re, im);
-				data.push_back(die);
-			}
-		}
-		SqMatrix<TRational> sq(batch_size, data);
-		answer += "определитель рациональной матрицы - ";
-		s << sq.findDet();
-		answer += s; }
-		break;
-	
-	case 4: {//rank for double
+
+	if ((mode % 3)==1) {
 		std::vector<myDouble> data;
 		for (int i = 2; i < 2 + batch_size * batch_size; i++) {
 			myDouble d = splitted.at(i).toDouble();
@@ -80,13 +27,22 @@ void TServerInterface::receive(QByteArray msg) {//msg should be like [size,mode,
 		}
 		SqMatrix<myDouble> sq(batch_size, data);
 		std::cout << "processed data from request\n" << data.size() << endl;
-		answer += "ранг вещественной матрицы - ";
-		s += QString().setNum(sq.ranking());
-		answer += s;
-		std::cout << "Found rank - " << s.toStdString() << endl;
+		switch (mode)
+		{
+		case 4:
+			answer += "ранг вещественной матрицы - ";
+			s += QString().setNum(sq.ranking());
+			break;
+		case 7:
+			s = sq.transpose();
+			break;
+		default:
+			answer += "определитель вещественной матрицы - ";
+			s << sq.findDet();
+			break;
+		}
 	}
-		  break;
-	case 5: {//rank for complex
+	else if ((mode % 3) == 2) {
 		std::vector<TComplex> data;
 		for (int i = 2; i < 2 + batch_size * batch_size * 2; i++) {
 			if (i % 2 == 0) {
@@ -99,12 +55,22 @@ void TServerInterface::receive(QByteArray msg) {//msg should be like [size,mode,
 			}
 		}
 		SqMatrix<TComplex> sq(batch_size, data);
-		answer += "ранг комплексной матрицы - ";
-		s += QString().setNum(sq.ranking());
-		answer += s;
+		switch (mode)
+		{
+		case 5:
+			answer += "ранг комплексной матрицы - ";
+			s += QString().setNum(sq.ranking());
+			break;
+		case 8:
+			s = sq.transpose();
+			break;
+		default://2
+			answer += "определитель комплексной матрицы - ";
+			s << sq.findDet();
+			break;
+		}
 	}
-		break;
-	case 6: {//rank for rational
+	else {
 		std::vector<TRational> data;
 		for (int i = 2; i < 2 + batch_size * batch_size * 2; i++) {
 			if (i % 2 == 0) {
@@ -117,61 +83,22 @@ void TServerInterface::receive(QByteArray msg) {//msg should be like [size,mode,
 			}
 		}
 		SqMatrix<TRational> sq(batch_size, data);
-		answer += "ранг комплексной матрицы - ";
-		s += QString().setNum(sq.ranking());
-		answer += s;
-	}
-		break;
-	case 7:{//trans for double
-		std::vector<myDouble> data;
-		for (int i = 2; i < 2 + batch_size * batch_size; i++) {
-			myDouble d = splitted.at(i).toDouble();
-			std::cout << d.get() << endl;
-			data.push_back(d);
+		switch (mode)
+		{
+		case 6:
+			answer += "ранг комплексной матрицы - ";
+			s += QString().setNum(sq.ranking());
+			break;
+		case 9:
+			s = sq.transpose();
+			break;
+		default:
+			answer += "определитель рациональной матрицы - ";
+			s << sq.findDet();
+			break;
 		}
-		SqMatrix<myDouble> sq(batch_size, data);
-		std::cout << "processed data from request\n" << data.size() << endl;
-		s = sq.transpose();
-		answer += s;
+		
 	}
-		break;
-	case 8:{//trans for complex
-		std::vector<TComplex> data;
-		for (int i = 2; i < 2 + batch_size * batch_size * 2; i++) {
-			if (i % 2 == 0) {
-				re = splitted.at(i).toInt();
-			}
-			else {
-				im = splitted.at(i).toInt();
-				TComplex die(re, im);
-				data.push_back(die);
-			}
-		}
-		SqMatrix<TComplex> sq(batch_size, data);
-		s = sq.transpose();
-		answer += s;
-	}
-		break;
-	case 9:{//trans for rational
-		std::vector<TRational> data;
-		for (int i = 2; i < 2 + batch_size * batch_size * 2; i++) {
-			if (i % 2 == 0) {
-				re = splitted.at(i).toInt();
-			}
-			else {
-				im = splitted.at(i).toInt();
-				TRational die(re, im);
-				data.push_back(die);
-			}
-		}
-		SqMatrix<TRational> sq(batch_size, data);
-		s = sq.transpose();
-		answer += s;
-	}
-		break;
-	default:
-		answer = "error: incorrect mode value";
-		break;
-	}
+	answer += s;
 	comm->send(QByteArray().append(answer.toUtf8()));
 }
